90c4cbded0a680965076b781576c040c265aadc2
zeyorama <zeyorama@hotmail.de>
2012-11-21 14:32:08 +0100

    user login fixed

--- action/user/login.php
+++ action/user/login.php
@@ -6,16 +6,15 @@
   include '../../epic.php';
 ?>
 <?php
-  
+
   $errcode = User::login($_POST['user_username'], $_POST['user_pass']);
 
-  if (!$errcode) {
-    header("Location: ../../index.php?f=user&s=login&e=$errcode");
+  if ($errcode == 10000) {
+    header("Location: ../../index.php?f=user&s=profile&confirm=user_login");
     exit();
 
   }
 
-  header("Location: ../../index.php");
-    
+  header("Location: ../../index.php?f=user&s=login&e=$errcode");  
 
 ?>
