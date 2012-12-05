f2bf6c1299e3fa383eab46925dab6cf10732f814
zeyorama <zeyorama@hotmail.de>
2012-12-05 12:33:02 +0100

    added sysconst file for global constants

---
+++ lib/core/sysconst.php
@@ -0,0 +1,12 @@
+<?php
+  /**
+   * @author Frank Kevin Zey
+   */
+
+  # system information constants
+  define("SYS_NAME", "TournamentMaker");
+  define("SYS_VERSION", "0.0.0 Development");
+  define("SYS_BUILD", "26:30112012");
+
+  define("SYS_MOTTO", "We establish your events");
+?>
