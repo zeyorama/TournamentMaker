611ecf2671f797ad47a09c27eb8b4fb208141fff
zeyorama <fkz.dev@hotmail.de>
2013-04-18 17:34:30 +0200

    removed merge conflict on user profile page

--- partial/user/profile.php
+++ partial/user/profile.php
@@ -10,7 +10,6 @@
 
     $user = User::getUser($_GET['id']);
 
-<<<<<<< HEAD
     if (is_int($user)) {
       $err = $user;
 
@@ -23,16 +22,6 @@
           exit();
         
       }
-=======
-    switch ($user) {
-      
-      case 10001:
-      case 10002:
-      case 10004:
-        header("Location: index.php?e=$user");
-        exit();
-      
->>>>>>> c191e6e376f48e2fabefa91e64026ef282cf92a3
     }
 
   } else {
